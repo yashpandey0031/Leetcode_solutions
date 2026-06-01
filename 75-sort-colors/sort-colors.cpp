ff1517dc401a9f578000b1189d61199fb071ba6d
Yash Pandey <143602355+yashpandey0031@users.noreply.github.com>
2026-06-01 19:42:52 +0530

    Time: 0 ms (100.00%) | Memory: 11.8 MB (11.03%) - LeetSync

--- 75-sort-colors/sort-colors.cpp
+++ 75-sort-colors/sort-colors.cpp
@@ -1,9 +1,44 @@
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
-        sort(nums.begin(),nums.end());
-        for(int i = 0;i<nums.size();i++){
-            cout << nums[i] << " ";
+    int n = nums.size();
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = 0; j < n - i - 1; j++) {
+            if (nums[j] > nums[j+1]) {
+                swap(nums[j], nums[j+1]);
+            }
         }
     }
+}
 };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// class Solution {
+// public:
+//     void sortColors(vector<int>& nums) {
+//         sort(nums.begin(),nums.end());
+//         for(int i = 0;i<nums.size();i++){
+//             cout << nums[i] << " ";
+//         }
+//     }
+// }; // 
