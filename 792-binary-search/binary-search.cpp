710807ff2bad3d6029f1a456f7bdf816150ce495
Yash Pandey <143602355+yashpandey0031@users.noreply.github.com>
2026-06-03 19:36:02 +0530

    Time: 0 ms (100.00%) | Memory: 31.2 MB (80.21%) - LeetSync

--- 792-binary-search/binary-search.cpp
+++ 792-binary-search/binary-search.cpp
@@ -1,12 +1,23 @@
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
-        int target_position = -1;
-        for(int i = 0;i<nums.size();i++){
-            if(nums[i] == target){
-                target_position = i;
-            } 
-        } 
-        return target_position;     
+        int left = 0;
+        int right = nums.size() - 1;
+
+        while(left <= right){
+            int mid = left + (right - left)/2;
+
+            if(nums[mid] == target){
+                return mid;
+            }
+            else if(nums[mid] < target){
+                left = mid+1;
+            }
+
+            else {
+                right = mid-1;
+            }
+        }
+        return -1;
     }
 };
